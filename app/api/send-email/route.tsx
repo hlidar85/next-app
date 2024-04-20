a10c2981210d41ec3424ac00138f6e30941c5ecb
hlidar <hlidar85@gmail.com>
2024-04-20 20:10:28 +0000

    fix api keys

--- app/api/send-email/route.tsx
+++ app/api/send-email/route.tsx
@@ -2,7 +2,7 @@ import WelcomeTemplate from "@/emails/WelcomeTemplate";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend("re_WnJjSSyc_5r2XDYs4g8acMZqb29E2CXDU");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
   await resend.emails.send({
